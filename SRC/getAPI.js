b29b79d46c63382ecdac98881a6626157dcca2b5
GotoThelast <h.lafe@it-students.fr>
2023-03-22 15:45:30 +0100

    fix bug

--- SRC/getAPI.js
+++ SRC/getAPI.js
@@ -1,6 +1,123 @@
 
 // fonction pour récuperer la latitude et longitude de l'utilisateur
 
+function findMostFrequentValue(arr) {
+    let frequencies = {};
+    let maxFreq = 0;
+    let mostFrequentValue;
+
+    for (let i = 0; i < arr.length; i++) {
+        let currentValue = arr[i];
+        frequencies[currentValue] = (frequencies[currentValue] || 0) + 1;
+
+        if (frequencies[currentValue] > maxFreq) {
+            maxFreq = frequencies[currentValue];
+            mostFrequentValue = currentValue;
+        }
+    }
+
+    return mostFrequentValue;
+}
+
+
+
+function getWeatherDescription(code) {
+    let description = "";
+
+    switch (code) {
+        case 0:
+            description = "Ciel dégagé";
+            break;
+        case 1:
+            description = "Essentiellement dégagé";
+            break;
+        case 2:
+            description = "Partiellement nuageux";
+            break;
+        case 3:
+            description = "Couvert";
+            break;
+        case 45:
+            description = "Brouillard";
+            break;
+        case 48:
+            description = "Brouillard givrant";
+            break;
+        case 51:
+            description = "Bruine : Légère";
+            break;
+        case 53:
+            description = "Bruine : Modérée";
+            break;
+        case 55:
+            description = "Bruine : Forte intensité";
+            break;
+        case 56:
+            description = "Bruine verglaçante : Légère";
+            break;
+        case 57:
+            description = "Bruine verglaçante : Forte intensité";
+            break;
+        case 61:
+            description = "Pluie : Légère";
+            break;
+        case 63:
+            description = "Pluie : Modérée";
+            break;
+        case 65:
+            description = "Pluie : Forte intensité";
+            break;
+        case 66:
+            description = "Pluie verglaçante : Légère";
+            break;
+        case 67:
+            description = "Pluie verglaçante : Forte intensité";
+            break;
+        case 71:
+            description = "Neige : Légère";
+            break;
+        case 73:
+            description = "Neige : Modérée";
+            break;
+        case 75:
+            description = "Neige : Forte intensité";
+            break;
+        case 77:
+            description = "Grains de neige";
+            break;
+        case 80:
+            description = "Averses de pluie : Légères";
+            break;
+        case 81:
+            description = "Averses de pluie : Modérées";
+            break;
+        case 82:
+            description = "Averses de pluie : Violentes";
+            break;
+        case 85:
+            description = "Averses de neige : Légères";
+            break;
+        case 86:
+            description = "Averses de neige : Fortes";
+            break;
+        case 95:
+            description = "Orage : Léger ou modéré";
+            break;
+        case 96:
+            description = "Orage avec grêle légère";
+            break;
+        case 99:
+            description = "Orage avec grêle forte";
+            break;
+        default:
+            description = "Code inconnu";
+            break;
+    }
+
+    return description;
+}
+
+
 function getLocation() {
     // Get the current location of the user
     if (navigator.geolocation) {
@@ -48,6 +165,18 @@ function getCityName(lat, lng) {
     console.log(urlMeteo)
     // Fetch the data from the URL
 
+
+
+    document.getElementById('tableHours').innerHTML = "<td class=\"cellHours\">Heures</td>" ;
+    document.getElementById('tableTemperature').innerHTML = "<td class=\"cellHours\">Température (°C)</td>" ;
+    document.getElementById('tableRain').innerHTML = "<td class=\"cellHours\">Pluie (mm)</td>" ;
+    document.getElementById('tableCloud').innerHTML = "<td class=\"cellHours\">Nuages (%)</td>" ;
+    document.getElementById('tableSnow').innerHTML = "<td class=\"cellHours\">Neige (cm)</td>" ;
+
+
+
+
+
     fetch(urlMeteo)
         .then(response => response.json())
         .then(data => {
@@ -62,6 +191,12 @@ function getCityName(lat, lng) {
             let newLocalHours = localHours.split(":");
             let sky = data.hourly.weathercode;
             // Loop through the hours
+
+            let temperatureList2 = []
+            let temperatureList1 = []
+            let weathercodeList1 = []
+            let weathercodeList2 = []
+
             for(i = 0; i<hours.length; i++){
                 let time = hours[i].split('T');
                 let checkDay = time[0].split('-');
@@ -219,6 +354,8 @@ function getCityName(lat, lng) {
                     temperatureList1.push(temperature[i])
                     if (temperatureList1.length == 24){
                         console.log("J+1 temperature max "+Math.max(...temperatureList1))
+                        document.getElementById('previsionsDay').innerHTML += "<br><div> "+ Math.max(...temperatureList1) +"</div>"
+
                         console.log("J+1 temperature min "+Math.min(...temperatureList1))
                     }
                     if (weathercodeList1.length == 24){
