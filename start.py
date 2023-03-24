8cd7557d3cabc21db7f0443962149d62c2a10ce2
Oosewalds <m.fournier@it-students.fr>
2023-03-24 11:04:47 +0100

    execution html firefox

--- start.py
+++ start.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import sys
 import os
-
 os.environ['DISPLAY'] = ':0'
 
 sys.path.append('/home/pi/.local/lib/python3.9/site-packages')
@@ -23,3 +22,29 @@ subprocess.Popen(['firefox-esr','--no-sandbox', 'http://localhost:8000/'])
 
 pyautogui.hotkey('cmd', 'O')
 pyautogui.hotkey('ctrl', 'cmd', 'f')
+
+#Code chatGPT
+#import os
+import subprocess
+import webbrowser
+
+# Définir le port du serveur
+PORT = 8000
+
+# Chemin vers le fichier HTML
+FILE_PATH = 'index.html'
+
+# Lancer le serveur PythOn
+try:
+    subprocess.Popen(['python', '-m', 'http.server', str(PORT)])
+except Exception as e:
+    print(f"Erreur lors du lancement du serveur: {e}")
+    exit()
+
+# Ouvrir le navigateur web et afficher la page HTML
+try:
+    browser = webbrowser.get('firefox')
+    browser.open(f'http://localhost:{PORT}/{FILE_PATH}')
+except Exception as e:
+    print(f"Erreur lors de l'ouverture du navigateur: {e}")
+    exit()
